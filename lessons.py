464ddfad8496d412f8be524ae16b55964a58ac0f
drakehitch <filin140123@gmail.com>
2022-01-17 04:28:15 +0500

    done lesson about args and kwargs

--- lessons.py
+++ lessons.py
@@ -3,8 +3,19 @@ def is_cat_here(*args):
         return True
     return False
 
-def is_item_here(item,*args):
+
+def is_item_here(item, *args):
     if item in args:
         return True
     return False
 
+
+def your_favorite_color(my_color, **kwargs):
+    if "color" in kwargs:
+        print(f"My favorite color is {my_color}, but {kwargs['color']} is also pretty good!")
+    else:
+        print(f"My favorite color is {my_color}, what is your favorite color?")
+
+
+# your_favorite_color('red', color="blue", weight=25)
+# your_favorite_color('white', shape="round")
