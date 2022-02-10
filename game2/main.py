a62673da3d9b5510c382654d7ae8a8aef63f30f2
drakehitch <filin140123@gmail.com>
2022-02-10 11:01:20 +0500

    map update and optimization

--- game2/main.py
+++ game2/main.py
@@ -7,14 +7,10 @@ while True:
     cmap = map_class.current_map()
     gmap = map_class.game_map
     print(cmap)
-    print(gmap)
     action = input("Where do you want to go?: ")
-    if action == "up":
-        print(map_class.calc_chance_on_map(gmap["up"]))
-    elif action == "left":
-        print(map_class.calc_chance_on_map(gmap["left"]))
-    elif action == "right":
-        print(map_class.calc_chance_on_map(gmap["right"]))
-    elif action in ["quit", "exit", ""]:
+    for i in map_class.map_instance.DIRECTIONS:
+        if action == i:
+            print(map_class.calc_chance_on_map(gmap[i]))
+    if action in ["quit", "exit", ""]:
         break
     map_class = reload(map_class)
--- game2/map_class.py
+++ game2/map_class.py
@@ -4,7 +4,7 @@ from prettytable import PrettyTable, DOUBLE_BORDER
 
 class MapGenerator:
     __TOTAL_CHANCE = 100
-    __DIRECTIONS = ["up", "left", "right"]
+    DIRECTIONS = ["up", "left", "right", "down"]
 
     @property
     def generate_tile(self):
@@ -14,22 +14,26 @@ class MapGenerator:
         return [enemy_chance, treasure_chance, empty_chance]
 
     def get_tiles(self):
-        return {d: self.generate_tile for d in self.__DIRECTIONS}
+        return {d: self.generate_tile for d in self.DIRECTIONS}
 
 
 map_instance = MapGenerator()
 
 game_map = map_instance.get_tiles()
-list_map = [f"{k}:\n{v[0]}% enemy\n{v[1]}% treasure\n{v[2]}% empty" for k, v in game_map.items()]
-printable_map = [["", list_map[0], ""], [list_map[1], "You are here", list_map[2]]]
+list_map = [f"{k}:\n"
+            f"{v[0]}% enemy\n"
+            f"{v[1]}% treasure\n"
+            f"{v[2]}% empty" for k, v in game_map.items()]
+printable_map = [["",          list_map[0],      ""],
+                 [list_map[1], "\nYou are here", list_map[2]],
+                 ["",          list_map[3],      ""]]
 
 
 def current_map():
     p = PrettyTable()
     p.set_style(DOUBLE_BORDER)
 
-    pmap = printable_map
-    for row in pmap:
+    for row in printable_map:
         p.add_row(row)
 
     return p.get_string(header=False, hrules=True)
@@ -46,5 +50,5 @@ def calc_chance_on_map(direction):
 
 
 if __name__ == "__main__":
-    calc_chance_on_map(direction)
     current_map()
+    calc_chance_on_map(direction)
