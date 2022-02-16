5a53b3197e924b810b5d19cdb27cae3e269de741
drakehitch <filin140123@gmail.com>
2022-02-16 17:28:34 +0500

    scrape init

---
+++ scraping/scr1.py
@@ -0,0 +1,9 @@
+from bs4 import BeautifulSoup
+
+with open("test.html", "r", encoding="utf-8") as myhtml:
+    parsed = BeautifulSoup(myhtml, 'html.parser')
+    # print(parsed)
+    # print(type(parsed))
+    print(parsed.body.h3)
+    for i in parsed.find_all('li'):
+        print(i)
