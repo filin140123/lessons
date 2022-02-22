eab570df9e51bf7f0bf78f629ca2a4a04223a102
drakehitch <filin140123@gmail.com>
2022-02-22 20:34:24 +0500

    csv update 20:34

--- csvfiles/quotes_scraping_to_csv.py
+++ csvfiles/quotes_scraping_to_csv.py
@@ -6,11 +6,20 @@ response = requests.get("http://quotes.toscrape.com/")
 html_data = BeautifulSoup(response.text, features="lxml")
 quotes = html_data.find_all(class_='quote')
 
-with open('quotes.csv', 'w', encoding='utf-8') as file:
-    csv_writer = csv.writer(file)
-    csv_writer.writerow(['Text', 'Author', 'Tags'])
-    for q in quotes:
-        text = q.find(class_="text").get_text()
-        author = q.find(class_="author").get_text()
-        tags = q.find(class_="keywords")['content']
-        csv_writer.writerow([text, author, tags])
+# with open('quotes.csv', 'w', encoding='utf-8') as file:
+#     csv_writer = csv.writer(file)
+#     csv_writer.writerow(['Text', 'Author', 'Tags'])
+#     for q in quotes:
+#         text = q.find(class_="text").get_text()
+#         author = q.find(class_="author").get_text()
+#         tags = q.find(class_="keywords")['content']
+#         csv_writer.writerow([text, author, tags])
+
+with open('quotes.csv', 'r', encoding='utf-8') as file:
+    csv_reader = csv.reader(file)
+    next(csv_reader)
+    for q in csv_reader:
+        if q:
+            print(q[0])
+            print(f"--- {q[1].upper()} --- tags: {', '.join(q[2].split(','))}")
+            print("-" * 60)
